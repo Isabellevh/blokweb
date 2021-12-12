747c0f26e3ce33fa07b67bc5e6e6af46a45f93bd
Isabellevh <94197902+Isabellevh@users.noreply.github.com>
2021-12-12 17:04:10 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,9 @@ var menu = document.querySelector('.menu');
 var openButton = document.querySelector('.open');
 var closeButton = document.querySelector('.close');
 
-
 openButton.addEventListener('click', toggleMenu);
 closeButton.addEventListener('click', toggleMenu);
 
-
 function toggleMenu() {
 menu.classList.toggle('menu-open');
-
 }
